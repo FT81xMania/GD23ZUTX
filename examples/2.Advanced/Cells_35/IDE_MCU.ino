9709f103844ea7ce78ded2fc0389c521ed2f7866
FT81x Manía <ft81xmania@gmail.com>
2019-12-05 09:40:53 -0600

    Update IDE_MCU.ino

--- examples/2.Advanced/Cells_35/IDE_MCU.ino
+++ examples/2.Advanced/Cells_35/IDE_MCU.ino
@@ -54,11 +54,10 @@ void Parametros()
 {
   GD.SaveContext();
    sprintf(TXP,"F_CPU: %d MHz", (F_CPU/1000000));  
-   GD.ColorRGB(0x00ff00); GD.cmd_text(GD.w-365, 3, 20, 0, TXP);
-   GD.ColorRGB(0xffff00); GD.cmd_text(GD.w-273, 3, 20, 0, Board);
+   GD.ColorRGB(0x00ff00); GD.cmd_text((GD.w/2)-40, 3, 20, 0, TXP);
+   GD.ColorRGB(0xffff00); GD.cmd_text(3, 3, 20, 0, Board);
 
-   GD.ColorRGB(0xffffff); GD.cmd_text((GD.w / 2), (GD.h)-20, 30, OPT_CENTER, "Matrix Orbital EVE3 TFT 5'' BT815");
-   GD.ColorRGB(0x444444); GD.cmd_text((GD.w-40), (GD.h)-10, 20, OPT_CENTER, "FT81xmania Team");
+   GD.ColorRGB(0x888800); GD.cmd_text(3, (GD.h)-10, 20, OPT_CENTER, "FT81xmania Team");
 
   GD.RestoreContext();
 }
