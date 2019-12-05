440c8cf506a37757ae3e6d1ae2834450851a7609
FT81x Manía <ft81xmania@gmail.com>
2019-12-05 09:18:40 -0600

    Add files via upload

---
+++ examples/2.Advanced/Cells_35/IDE_MCU.ino
@@ -0,0 +1,64 @@
+char Board[40];
+int ClaveBoard;
+
+void IDEMCU()
+{
+     ClaveBoard=0;
+     sprintf(Board, "MCU: None               (%d)", ClaveBoard);
+
+  #if defined(ARDUINO_ARCH_STM32)
+     ClaveBoard=1;
+     //sprintf(Board, "MCU(%d): STM32 Nucleo-F767ZI", ClaveBoard);
+     //if (STM32_CPU==407){sprintf(Board, "MCU: STM32 F407 M4DEMO"); MCUID=407;}
+     if (STM32_CPU==4070){sprintf(Board, "MCU: STM32 F407 M4DEMO_SD"); MCUID=4070;}
+     if (STM32_CPU==4071){sprintf(Board, "MCU: STM32 F407 M4DEMO_SDIO"); MCUID=4071;}
+     if (STM32_CPU==429){sprintf(Board, "MCU: STM32 F49I-DISCO"); MCUID=429;}
+     if (STM32_CPU==746){sprintf(Board, "MCU: STM32 Core7XXI"); MCUID=746;}
+     if (STM32_CPU==767){sprintf(Board, "MCU: STM32 Nucleo-F767ZI (Core Danieleff)"); MCUID=767;}
+     if (STM32_CPU==7670){sprintf(Board, "MCU: STM32 Nucleo-F767ZI (Core Oficial)"); MCUID=7670;}
+  #endif
+
+  #ifdef TEENSYDUINO
+     ClaveBoard=2;
+    #if defined(__IMXRT1052__) || defined(__IMXRT1062__)
+      sprintf(Board, "MCU: Teensy 4");
+    #else
+      sprintf(Board, "MCU: Teensy 3.X");
+    #endif
+  #endif
+
+  #ifdef __DUE__
+     ClaveBoard=3;
+     sprintf(Board, "MCU: Arduino Due");
+  #endif
+
+  #ifdef ESP8266
+     ClaveBoard=4;
+     sprintf(Board, "MCU: ESP8266");
+  #endif
+
+  #if defined(ESP32)
+     ClaveBoard=5;
+     sprintf(Board, "MCU: ESP32");
+  #endif
+
+  #if !defined(TEENSYDUINO)&& !defined(ESP32)&& !defined(__DUE__)&& !defined(ESP8266)&& !defined(ARDUINO_ARCH_STM32)&& defined(ARDUINO)
+     ClaveBoard=6;
+     sprintf(Board, "MCU: Arduino MEGA/UNO");
+  #endif
+}
+
+char TXP[50];
+
+void Parametros()
+{
+  GD.SaveContext();
+   sprintf(TXP,"F_CPU: %d MHz", (F_CPU/1000000));  
+   GD.ColorRGB(0x00ff00); GD.cmd_text(GD.w-365, 3, 20, 0, TXP);
+   GD.ColorRGB(0xffff00); GD.cmd_text(GD.w-273, 3, 20, 0, Board);
+
+   GD.ColorRGB(0xffffff); GD.cmd_text((GD.w / 2), (GD.h)-20, 30, OPT_CENTER, "Matrix Orbital EVE3 TFT 5'' BT815");
+   GD.ColorRGB(0x444444); GD.cmd_text((GD.w-40), (GD.h)-10, 20, OPT_CENTER, "FT81xmania Team");
+
+  GD.RestoreContext();
+}
---
+++ examples/2.Advanced/Cells_35/pez1.h
@@ -0,0 +1,32 @@
+#define LOAD_ASSETS0()  GD.safeload("pez1.gd2");
+#define PEZ1D_HANDLE 0
+#define PEZ1D_WIDTH 200
+#define PEZ1D_HEIGHT 131
+#define PEZ1D_CELLS 1
+#define PEZ1I_HANDLE 1
+#define PEZ1I_WIDTH 200
+#define PEZ1I_HEIGHT 131
+#define PEZ1I_CELLS 1
+#define PEZ1C_HANDLE 2
+#define PEZ1C_WIDTH 178
+#define PEZ1C_HEIGHT 178
+#define PEZ1C_CELLS 1
+#define BPEZ1_HANDLE 3
+#define BPEZ1_WIDTH 75
+#define BPEZ1_HEIGHT 80
+#define BPEZ1_CELLS 1
+#define BPEZ2_HANDLE 4
+#define BPEZ2_WIDTH 75
+#define BPEZ2_HEIGHT 80
+#define BPEZ2_CELLS 1
+#define BPEZ3_HANDLE 5
+#define BPEZ3_WIDTH 40
+#define BPEZ3_HEIGHT 40
+#define BPEZ3_CELLS 1
+#define ASSETS0_END 195368UL
+static const shape_t PEZ1D_SHAPE = {0, 200, 131, 0};
+static const shape_t PEZ1I_SHAPE = {1, 200, 131, 0};
+static const shape_t PEZ1C_SHAPE = {2, 178, 178, 0};
+static const shape_t BPEZ1_SHAPE = {3, 75, 80, 0};
+static const shape_t BPEZ2_SHAPE = {4, 75, 80, 0};
+static const shape_t BPEZ3_SHAPE = {5, 40, 40, 0};
