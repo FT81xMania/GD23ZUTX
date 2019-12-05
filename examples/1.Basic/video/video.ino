e1ca4a03d554ea56de352bebb4d71dde478f1ab7
FT81x Manía <ft81xmania@gmail.com>
2019-12-05 08:41:51 -0600

    Update video.ino

--- examples/1.Basic/video/video.ino
+++ examples/1.Basic/video/video.ino
@@ -1,17 +1,13 @@
-//#include <GD23ZU.h>
-
-#include <SPI.h>
-#include <STM32_GD2.h>
+#include <GD23ZUTX.h>
 
 void setup()
 {
-  //GD.begin(GD_STORAGE);
   GD.begin();
 }
 void loop()
 {
   MoviePlayer mp;
 
-  mp.begin("JS_Cndy.avi");
+  mp.begin("oficial.avi");
   mp.play();
 }
