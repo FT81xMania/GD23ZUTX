c56045f7b5095408612626fa201479dae4092c55
FT81x Manía <ft81xmania@gmail.com>
2020-01-22 11:37:23 -0600

    Add files via upload

--- examples/1.Basic/JPG_load/JPG_load.ino
+++ examples/1.Basic/JPG_load/JPG_load.ino
@@ -1,5 +1,6 @@
 #include <GD23ZUTX.h>
 
+char TXP[50];
 char LoadTime[40];
 uint32_t startJPG=0, endJPG=0, drawT=0;
 
@@ -34,6 +35,12 @@ void MP()
     GD.ColorRGB(0xff,0x00,0x00);
     GD.cmd_text(GD.w/2, (GD.h)-20, 29, OPT_CENTER, LoadTime);
    GD.RestoreContext();    
+
+sprintf(TXP,"F_CPU: %d MHz", (F_CPU/1000000));  GD.cmd_text(GD.w-43, (GD.h)-20, 20, OPT_CENTER, TXP);
+sprintf(TXP,"F_BUS: %d MHz", (F_BUS/1000000));  GD.cmd_text(GD.w-43, (GD.h)-7, 20, OPT_CENTER, TXP);
+
+sprintf(TXP,"SPI speed: %d MHz", (SetSPISpeed/1000000));  GD.cmd_text(GD.w-45, (GD.h)-36, 20, OPT_CENTER, TXP);
+
    
    GD.swap();
  }
